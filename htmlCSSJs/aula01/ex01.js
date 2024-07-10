6b42278d92a35ca1698697370a470902c0db73bc
Victor Albuquerque <165061558+albqvictor1508@users.noreply.github.com>
2024-07-10 12:11:21 -0300

    commit

--- htmlCSSJs/aula01/ex01.js
+++ htmlCSSJs/aula01/ex01.js
@@ -1,39 +1,52 @@
-
-function pegarSection() {
-    const div = ExibirNoHtml();
-    console.log(div);
-    const section = document.querySelector('section');
-    section.innerHTML = div;
+const criaAtividades = () => {
+    const atividades = [
+        {
+            nome: 'Academia',
+            data: new Date("2024-07-20 10:00"),
+            concluido: false,
+        },
+        {
+            nome: 'Jantar',
+            data: new Date("2024-09-30 09:00"),
+            concluido: true,
+        },
+        {
+            nome: 'Codar',
+            data: new Date(),
+            concluido: false,
+        }
+    ];
+    for(let atividade in atividades) {
+        criaItemDaAtividade(atividades[atividade]);
+    }
 }
-pegarSection();
+criaAtividades();
 
-function criaObjeto() {
-    const data = new Date("2024-08-15 12:00");
-    const obj = new Object();
-    obj.nome = 'alexsa';
-    obj.sobrenome = 'delicia';
-    obj.horas = data.getHours();
-    obj.finalizou = false;
-    return obj;
-}
-
-function ExibirNoHtml() {
-    const obj = criaObjeto();
-    let input = `<input type="checkbox"/>`;
+function criaItemDaAtividade(atividade) {
     
-    if (obj.horas === 12) {
-        obj.finalizou = true;
+    function inputCheckBox() {
+        let input = `<input type="checkbox" id="checkBox"/>`;
+
+        if(atividade.concluido) {
+            input = `<input type="checkbox" id="checkBox" checked/>`;
+        }
+        return input;
     }
-    if (obj.finalizou) {
-        input = `<input type="checkbox" checked/>`;
-    }
-    return `
+
+    const input = inputCheckBox();
+    const itemAtividade =  `
     <div>
         <p>
             ${input}
-            <span>${obj.nome} ${obj.sobrenome}</span> sai amanhã às ${obj.horas}
-            </p>
-            </div>    
-            `
-        }
-        
+            <span>${atividade.nome}</span>
+            <time>${atividade.data}</time>
+        </p>
+    </div>
+    `
+    ExibirAtividade(itemAtividade)
+}
+
+function ExibirAtividade(atividade) {
+    const section = document.querySelector('section');
+    section.innerHTML += atividade;
+}
